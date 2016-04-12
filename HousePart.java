592529208fa975b4339494433f634832b484e01d
kebeclibre <lcsperais@gmail.com>
2016-04-12 16:43:22 +0200

    surface Manage

--- HousePart.java
+++ HousePart.java
@@ -1,4 +1,12 @@
 abstract class HousePart {
 	private int surface;
+
+	public int getSurface() {
+		return surface;
+	}
+
+	public void setSurface(int surface) {
+		this.surface = surface;
+	}
 	
 }
